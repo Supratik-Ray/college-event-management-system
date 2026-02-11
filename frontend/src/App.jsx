e799b5b5dd5ddf60dafd9174af2aa679c98a7fc9
Supratik Ray <raysupratik8@gmail.com>
2026-02-11 20:54:46 +0530

    package.json change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,6 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
-
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/createevent" element={<CreateEvent />} />
